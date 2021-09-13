f63613211f58d15fd979b5987d1c9b93db47f157
Aarion Ojeda <aarion.ojeda01@gmail.com>
2021-09-13 15:44:21 -0500

    fixed attribute holdover

--- src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
+++ src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
@@ -21,6 +21,8 @@ public class RegisterServlet extends HttpServlet {
         }
 
         request.getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
+
+        request.getSession().removeAttribute("invalidregistration");
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
