d8146901e49a739c03afcbedef6e6e16fa58ed48
Michael Coyle <michael.david.coyle4@gmail.com>
2021-09-14 14:10:48 -0500

    Updated insert method to use Password class to hash password.

--- src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -2,6 +2,7 @@ package com.codeup.adlister.dao;
 
 import com.codeup.adlister.Config;
 import com.codeup.adlister.models.User;
+import com.codeup.adlister.util.Password;
 import com.mysql.cj.jdbc.Driver;
 import org.mindrot.jbcrypt.BCrypt;
 
@@ -54,7 +55,7 @@ public class MySQLUsersDao implements Users {
 
         try {
             PreparedStatement stmt = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
-            String hashedPw = BCrypt.hashpw(user.getPassword(), BCrypt.gensalt());
+            String hashedPw = Password.hash(user.getPassword());
             stmt.setString(1, user.getUsername());
             stmt.setString(2, user.getEmail());
             stmt.setString(3, hashedPw);
